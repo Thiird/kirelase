6bb486efcb4e23e773a6310db76bed47cd565983
Thiird <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>
2022-03-06 13:27:23 +0100

    sch and step export works

--- kirelease.py
+++ kirelease.py
@@ -3,9 +3,10 @@
 from asyncio import subprocess
 from ctypes import ArgumentError
 from genericpath import isdir
+from platform import release
 import sys
 from os import listdir, getcwd, mkdir, path, system
-from os.path import isfile, join
+from os.path import isdir, join
 import time
 from xvfbwrapper import Xvfb
 
@@ -15,50 +16,68 @@ from utils import (
 )
 
 RELEASE_DIRECTORY_NAME = 'release'
+pcb_file = ''
 
-def getReleaseNumber(outputDir):
-    releaseNumber = 0
+def getReleaseNumber(outDir):
+    nextRelease = -1
 
-    onlyfiles = [f for f in listdir(outputDir) if isfile(join(outputDir, f))]
+    for f in listdir(outDir): # expected format is 'project_rev_0', 'project_rev_1', ...
+        temp = f.split('_')
+        temp = int(temp[len(temp) - 1])
+        if temp > nextRelease:
+            nextRelease = temp
 
-    print(onlyfiles)
+    if nextRelease == -1:
+        return 0
+    else:
+        return nextRelease + 1
 
-    return releaseNumber
 
-def export_step():
-    a = system('kicad2step')
+def export_step(cwd, outputDir):
+    pcbFile = ''
+    for f in listdir(cwd):
+        if '.kicad_pcb' in f:
+            pcbFile = f.split('/')
+            pcbFile = pcbFile[len(pcbFile) - 1]
+            break 
+    with PopenContext(['kicad2step', pcbFile , '-o' + outputDir + '/model']) as kicad2step:
+        time.sleep(2)
+        kicad2step.terminate()
+    print("Exported 3D .step model in: " + outputDir + '/model')
 
-    print(a)
 
 def export_gerbers():
     print()
 
 def export_schematic(mainSchFile, outputDir):
-    with Xvfb(width=800, height=600, colordepth=24):
-        with PopenContext(['eeschema', mainSchFile]) as eeschema:
-            time.sleep(2)
-            # search for eeschema window and focus it
-            xdotool(['search', '--name', 'Schematic Editor'])
+    #with Xvfb(width=1280, height=720, colordepth=24):
+    with PopenContext(['eeschema', mainSchFile]) as eeschema:
+        time.sleep(0.3)
+        # search for eeschema window and focus it
+        xdotool(['search', '--name', 'Schematic Editor'])
+        time.sleep(0.3)
 
-            # open plotting window
-            xdotool(['key' ,'shift+ctrl+p'])
-            time.sleep(1)
+        # open plotting window
+        xdotool(['key' ,'shift+ctrl+p'])
+        time.sleep(0.3)
 
-            # search for plotting window and focus it
-            xdotool(['search', '--name', 'Plot Schematic Options'])
+        # search for plotting window and focus it
+        xdotool(['search', '--name', 'Plot Schematic Options'])
+        time.sleep(0.3)
 
-            # input output directory
-            xdotool(['key' , outputDir])
+        # input output directory
+        xdotool(['type' , outputDir + '/schematic.pdf'])
+        time.sleep(0.3)
 
-            # move to plot button by tabbing and press enter
-            cmd = ['key']
-            for x in range(1,19):
-                xdotool(['key','Tab'])
-            xdotool(['key','enter'])
+        # move to plot button by tabbing and press enter
+        for x in range(1,19):   
+            xdotool(['key','Tab'])
+        xdotool(['key','enter'])
 
-            # wait for export to take place, 2 seconds should suffice
-            time.sleep(2)
-            eeschema.terminate()
+        # wait for export to take place, 2 seconds should suffice
+        time.sleep(2)
+        eeschema.terminate()
+        print("Exported schematic .pdf in: " + outputDir + '/schematic.pdf')
 
 def export_bom():
     print()
@@ -71,9 +90,10 @@ if __name__ == '__main__':
     
     if (sys.argv[1] == 'help'):
         print("From the main directory of your KiCad project, use:\n\n"\
-            "Kirelease main.sch\n\n"
+            "Kirelease <root>.sch <project_name>\n\n"
             "where main.sch is the root sheet in the schematic hirearchy\n"\
-            "If a 'release' folder is not present, one will be created.")
+            "If a 'release' folder is not present, one will be created.\n"\
+            "If <project_name> is not specified, the name of the root.sch file will be used as the project name")
         exit(0)
 
     cwd = getcwd()
@@ -81,17 +101,30 @@ if __name__ == '__main__':
 
     # check for release folder
     if not path.isdir(outputDir):
-        mkdir(RELEASE_DIRECTORY_NAME)
-    
-    projectName = sys.argv[1].split("/")
-    projectName = projectName[len(projectName) - 1].split(".")[0]
+        print("No 'realease' directory available, create one: (y/n)")
+        choice = input().lower()
+        if choice in ['y','n']:
+            if choice == 'n':
+                print("ok nevermind then!")
+                exit(0)
+            else:
+                print("'release' directory created")
+                mkdir(RELEASE_DIRECTORY_NAME)
 
-    releaseNumber = getReleaseNumber(outputDir)        
+    fileToOpen = sys.argv[1].split("/")
+    fileToOpen = fileToOpen[len(fileToOpen) - 1].split(".")[0]
+
+    # if not given as argument, take project name from root .sch file
+    if (len(sys.argv) == 3):
+        schPath = sys.argv[2]
+
+    releaseNumber = getReleaseNumber(outputDir)     
+    outputDir = outputDir + '/' + fileToOpen + '_' + str(releaseNumber)
 
     if not path.isdir(outputDir):
         mkdir(outputDir)
 
-    export_schematic(outputDir)
-    export_step()
-    export_gerbers()
-    export_bom()
+    export_schematic(schPath, outputDir)
+    export_step(cwd, outputDir)
+    # export_gerbers()
+    # export_bom()
